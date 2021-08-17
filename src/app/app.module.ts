2378be64f0f3e879698b8968f31e88aaf00e7da3
diffethi <58692855+diffethi@users.noreply.github.com>
2021-08-17 13:38:15 +0200

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageredirectionComponent } from './pageredirection/pageredirection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageredirectionComponent
   ],
   imports: [
     BrowserModule,
