06093be45190e3d0ea24722265dbbbae67cbd6c8
EnzoCALL <enzo.12.juin@gmail.com>
2021-08-17 14:58:18 +0200

    Fully merging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { PageredirectionComponent } from './pageredirection/pageredirection.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagePresentationComponent } from './page-presentation/page-presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"page-presentation", component:PagePresentationComponent}, {path:"pageredirection", component:PageredirectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
