38ae123c418751b9c72933ac1df550c4794656b2
lcgLassalle <l.lassalle02@gmail.com>
2021-08-17 12:10:53 +0200

    creation page de presentation
    creation du component pour la page de presentation du groupe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagePresentationComponent } from './page-presentation/page-presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagePresentationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-presentation/page-presentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-presentation',
+  templateUrl: './page-presentation.component.html',
+  styleUrls: ['./page-presentation.component.css']
+})
+export class PagePresentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
